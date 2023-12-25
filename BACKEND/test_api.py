b7b786009678e49fd266f02e248bdd9dc37f0408
shynerdyninja <kananamelaine@gmail.com>
2023-12-25 17:19:43 +0300

    unit testing for creating,updating and deleting recipe

--- BACKEND/test_api.py
+++ BACKEND/test_api.py
@@ -2,7 +2,6 @@ import unittest
 from main import create_app
 from config import TestConfig
 from exts import db
-from models import Recipe
 
 
 class APITestCase(unittest.TestCase):
@@ -86,7 +85,7 @@ class APITestCase(unittest.TestCase):
         
         status_code = get_one_response.status_code
         
-        print(status_code)
+        #print(status_code)
         
         self.assertEqual(status_code,404)
         
@@ -108,15 +107,137 @@ class APITestCase(unittest.TestCase):
             "password":"password"
         })
         
-        print(login_response.json)
+        #access_token = login_response.json["access_token"]
         
+        for item in login_response.json:
+            if "access_token" in item:
+                access_token = item["access_token"]
+                #print(access_token)
+                break 
+
+        create_recipe_response = self.client.post(
+            '/recipe/recipes',
+            json = {
+            "title":"Test cookie",
+            "description":" Test description"
+        },
+                                                  
+            headers = {
+                "Authorization header":f"Bearer{access_token}"
+            })
+        
+        status_code = create_recipe_response.status_code
+        
+        #print(create_recipe_response.json)
+        
+        self.assertEqual(status_code,201)
         
     
     def test_update_recipe(self):
-        pass
+        signup_response = self.client.post(
+            '/auth/signup',
+            json = {
+            "username":"testuser",
+            "email":"testemail@gmail.com",
+            "password":"password"
+        })
+        
+        
+        login_response = self.client.post(
+            '/auth/login',
+            json = {
+            "username":"testuser",
+            "password":"password"
+        })
+        
+        #access_token = login_response.json["access_token"]
+        
+        for item in login_response.json:
+            if "access_token" in item:
+                access_token = item["access_token"]
+                #print(access_token)
+                break 
+
+        create_recipe_response = self.client.post(
+            '/recipe/recipes',
+            json = {
+            "title":"Test cookie",
+            "description":" Test description"
+        },
+                                                  
+            headers = {
+                "Authorization header":f"Bearer{access_token}"
+            })
+        
+        status_code = create_recipe_response.status_code
+        
+        #id = 1
+        
+        updated_response = self.client.put(
+            f"/recipe/recipe/{id}",
+            json = {
+                "title":"Test cookie updated",
+                "description":"Test description"
+            },
+            headers = {
+                "Authorization":f"Bearer {access_token}"
+            }
+        )
+        
+        status_code = updated_response.status_code
+        
+        #print(updated_response.json)
+        
+        self.assertEqual(status_code, 404)
+        
     
     def test_delete_recipe(self):
-        pass
+        signup_response = self.client.post(
+            '/auth/signup',
+            json = {
+            "username":"testuser",
+            "email":"testemail@gmail.com",
+            "password":"password"
+        })
+        
+        
+        login_response = self.client.post(
+            '/auth/login',
+            json = {
+            "username":"testuser",
+            "password":"password"
+        })
+        
+        #access_token = login_response.json["access_token"]
+        
+        for item in login_response.json:
+            if "access_token" in item:
+                access_token = item["access_token"]
+                #print(access_token)
+                break 
+
+        create_recipe_response = self.client.post(
+            '/recipe/recipes',
+            json = {
+            "title":"Test cookie",
+            "description":" Test description"
+        },
+                                                  
+            headers = {
+                "Authorization header":f"Bearer{access_token}"
+            })
+        
+        deleted_response = self.client.delete(
+            f'/recipe/recipe/{id}',
+            headers = {
+                "Authorization":f"Bearer{access_token}"
+            })
+        
+        status_code = deleted_response.status_code
+
+        print(deleted_response.json)
+
+        self.assertEqual(status_code, 404)
     
     def tearDown(self):
         """
